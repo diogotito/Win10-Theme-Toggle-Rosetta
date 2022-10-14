420b8ebc64e9168991a8768c25a5fc535e0e85cc
diogotito <diogotitomarques@gmail.com>
2022-10-14 11:39:16 +0100

    Clean up Python script

--- toggle_darkmode.py
+++ toggle_darkmode.py
@@ -1,37 +1,27 @@
 import ctypes
-import enum
 from winreg import (HKEY_CURRENT_USER, KEY_ALL_ACCESS, ConnectRegistry,
                     OpenKey, QueryValueEx, SetValueEx)
 
 
-class Theme(enum.IntEnum):
-    DARK = 0
-    LIGHT = 1
-    question = property({
-        LIGHT: "such light",
-        DARK: "escuro"
-    }.__getitem__)
-
-    def toggle(self):
-        return Theme(1 - self)
+def prompt(text, title="", boxtype=0x1 | 0x40):
+    """Shows a dialog box with Ok and Cancel buttons.
+    Returns True if the user activated the Ok button."""
+    choice = ctypes.windll.user32.MessageBoxW(0, str(text), str(title), boxtype)
+    return choice == 1
 
 
-def prompt(text, title=""):
-    return ctypes.windll.user32.MessageBoxW(0, str(text), str(title), 1) == 1
+if __name__ == "__main__":
+    THEME_NAMES = ["Dark", "Light"]
+    HKCU = ConnectRegistry(None, HKEY_CURRENT_USER)
+    PERSONALIZE_KEY = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize"
+    APPS_USE_LIGHT_THEME = "AppsUseLightTheme"
 
+    # Get the preference
+    Personalize = OpenKey(HKCU, PERSONALIZE_KEY, 0, KEY_ALL_ACCESS)
+    (v, t) = QueryValueEx(Personalize, APPS_USE_LIGHT_THEME)
 
-HKCU = ConnectRegistry(None, HKEY_CURRENT_USER)
-PERSONALIZE_KEY = (
-    r"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize"
-)
-APPS_USE_LIGHT_THEME = "AppsUseLightTheme"
-
-Personalize = OpenKey(HKCU, PERSONALIZE_KEY, 0, KEY_ALL_ACCESS)
-
-(v, t) = QueryValueEx(Personalize, APPS_USE_LIGHT_THEME)
-cur_theme = Theme(v)
-
-
-if prompt(text=cur_theme.question,
-          title="toggle_darkmode.py"):
-    SetValueEx(Personalize, APPS_USE_LIGHT_THEME, 0, t, cur_theme.toggle())
+    # Prompt the user
+    text = f"Toggle from {THEME_NAMES[v]} to {THEME_NAMES[1 - v]} theme?"
+    if prompt(text=text, title="toggle_darkmode.py"):
+        # Toggle the preference
+        SetValueEx(Personalize, APPS_USE_LIGHT_THEME, 0, t, 1 - v)
