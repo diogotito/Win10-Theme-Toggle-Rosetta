256cf94de6d838aa09ea6dd4eefac2aeb3b16bcf
diogotito <diogotitomarques@gmail.com>
2022-10-18 12:47:18 +0100

    Prompt for change in PowerShell

--- PowerShell/switch-lightmode.ps1
+++ PowerShell/switch-lightmode.ps1
@@ -1,20 +1,42 @@
+using assembly System.Windows.Forms
+using namespace System.Windows.Forms
+
+$Pref = @{
+    Path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+    Name = "AppsUseLightTheme";
+}
+
+$ThemeNames = @{
+    $true = "Light";
+    $false = "Dark";
+}
+
 function Set-LightMode([bool] $light) {
-    Set-ItemProperty `
-        -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize `
-        -Name AppsUseLightTheme `
-        -Value ($light -as [int]) `
-        -Type Dword `
-        -Force
+    Set-ItemProperty @Pref -Value ($light -as [int]) -Type Dword -Force
 }
 
 function Get-LightMode() {
-    return (Get-ItemPropertyValue `
-        -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize `
-        -Name AppsUseLightTheme) -as [bool]
+    (Get-ItemPropertyValue @Pref) -as [bool]
 }
 
 function Switch-LightMode() {
     Set-LightMode (-not (Get-LightMode))
 }
 
-Switch-LightMode
+function Confirm([string] $question) {
+    $choice = [MessageBox]::Show(
+        $question,
+        "switch-lightmode.ps1",
+        [MessageBoxButtons]::OKCancel,
+        [MessageBoxIcon]::Information,
+        [MessageBoxDefaultButton]::Button1
+    )
+    $choice -eq [DialogResult]::OK
+}
+
+$cur = $ThemeNames[(Get-LightMode)]
+$alt = $ThemeNames[-not (Get-LightMode)]
+if (Confirm "Toggle theme from $cur to ${alt}?")
+{
+    Switch-LightMode
+}
